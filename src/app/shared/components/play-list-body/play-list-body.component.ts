f47464a548e78b64fd3de6a15a70ee0c3128910f
javiera izquierdo <jizquierdo.3it@bancochile.cl>
2025-01-03 15:37:42 -0300

    vista de favoritos con data mock en play-list-body y play-list-header

--- src/app/shared/components/play-list-body/play-list-body.component.ts
+++ src/app/shared/components/play-list-body/play-list-body.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as data1 from "../.././../data/canciones.json"
+import { TrackModel } from '@core/modules/tracks.model';
 
 @Component({
   selector: 'app-play-list-body',
   templateUrl: './play-list-body.component.html',
   styleUrls: ['./play-list-body.component.css']
 })
-export class PlayListBodyComponent {
+export class PlayListBodyComponent implements OnInit {
+  tracks: TrackModel[] = []
 
+
+  constructor() {}
+  ngOnInit(): void {
+    const { data }: any = (data1 as any).default
+    this.tracks = data;
+  }
 }
