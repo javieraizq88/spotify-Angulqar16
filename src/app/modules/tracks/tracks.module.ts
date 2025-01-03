f9489f421278eb37161ffac9a304a4b079ae4681
javiera izquierdo <jizquierdo.3it@bancochile.cl>
2025-01-03 09:38:05 -0300

    se pasa titulo desde seccion generica a tracks page

--- src/app/modules/tracks/tracks.module.ts
+++ src/app/modules/tracks/tracks.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { TracksRoutingModule } from './tracks-routing.module';
 import { TracksPageComponent } from './page/tracks-page/tracks-page.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 
 @NgModule({
@@ -11,7 +12,8 @@ import { TracksPageComponent } from './page/tracks-page/tracks-page.component';
   ],
   imports: [
     CommonModule,
-    TracksRoutingModule
+    TracksRoutingModule,
+    SharedModule
   ]
 })
 export class TracksModule { }
--- src/app/shared/components/seccion-generica/seccion-generica.component.ts
+++ src/app/shared/components/seccion-generica/seccion-generica.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-seccion-generica',
   templateUrl: './seccion-generica.component.html',
   styleUrls: ['./seccion-generica.component.css']
 })
-export class SeccionGenericaComponent {
+export class SeccionGenericaComponent implements OnInit {
+  @Input() title: string = ""
 
+  constructor() {}
+  ngOnInit(): void {
+    
+  }
 }
