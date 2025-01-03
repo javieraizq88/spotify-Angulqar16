acbaeacb726b5f7f127fb645d486ee35667de537
javiera izquierdo <jizquierdo.3it@bancochile.cl>
2025-01-03 09:55:30 -0300

    se muestra en tracks, nombres de la seccion generica - mock- y se crea trackmodel y artistmodel

--- src/app/modules/tracks/page/tracks-page/tracks-page.component.ts
+++ src/app/modules/tracks/page/tracks-page/tracks-page.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tracks-page',
   templateUrl: './tracks-page.component.html',
   styleUrls: ['./tracks-page.component.css']
 })
-export class TracksPageComponent {
-
+export class TracksPageComponent implements OnInit {
+  mockTrackList = [
+    {
+      name: "nombre track"
+    },
+    {
+      name: "nombre track"
+    },
+    {
+      name: "nombre track"
+    },
+  ]
+  constructor() {}
+  ngOnInit(): void {
+    
+  }
 }
